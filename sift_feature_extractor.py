8dad15f3f58bfc7079857dae8de20be8db91f61b
Xiaoling-Zhu <xzhu.edu@gmail.com>
2017-03-29 22:27:31 +0200

    for compatibility

--- sift_feature_extractor.py
+++ sift_feature_extractor.py
@@ -1,21 +1,23 @@
 import cv2
+from scipy.misc import imread
 
-def sift(img_name):
+def sift(image):
     """
     reference:
     install opencv : https://www.scivision.co/compiling-opencv3-with-extra-contributed-modules/
     sift : http://www.pyimagesearch.com/2015/07/16/where-did-sift-and-surf-go-in-opencv-3/
 
     """
-
-    image = cv2.imread(img_name)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
-    (kps, descs) = sift.detectAndCompute(gray, None)
-    print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
+    (kps, descs) = sift.detectAndCompute(image, None)
+    #print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
+    
     return kps, descs
     
 if __name__ == '__main__':
-    img = 'data/image_0001.jpg'
+    img = cv2.imread('data/image_0001.jpg')
     
     kps, descs = sift(img)
+
+
