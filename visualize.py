d042b3508241493da75aaafd78c68d50287d4266
Xiaoling-Zhu <xzhu.edu@gmail.com>
2017-04-02 17:58:31 +0200

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -3,25 +3,43 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def visualize(ker, label, threshold=0.6):
+def visualize(ker, threshold=0.3):
     
-    """
+    '''
     L is the output kernel
     labels come form caltech_101_classes.txt
-    """
+    singletons are omitted from the graph
+    '''
     G = nx.Graph()
+    weight = {}
+    
+    #initialize the weights            
+    for i in range(len(ker)):
+        a = list(ker[i])
+        del a[i]
+        if np.max(a) > threshold:
+            weight[i] = {}
+            for j in range(i+1,len(ker)):
+                if ker[i][j] > threshold:
+                    weight[i][j] = ker[i][j]
+    print(weight.keys())
     
     #add nodes
-    G.add_nodes_from(list(range(len(ker))))
-
+    G.add_nodes_from(list(weight.keys()))
+    
     #add edges if similarity > 0.5
-    for i in range(len(ker)):
-        for j in range(i+1,len(ker)):
-            if ker[i][j] > threshold:
-                G.add_edges_from([(i,j,{'weight':ker[i][j]})])
+    for i in weight.keys():
+        for j in weight[i].keys():
+            G.add_edges_from([(i,j,{'weight':ker[i][j]})])
     pos = nx.spring_layout(G)
 
-    #draw the graph
+    #load labels
+    file = open('labels/caltech_101_classes.txt','r')
+    lines = file.readline().strip().split(',')
+    lines = [lines[i] for i in range(len(lines)) if i in weight.keys()]
+    label = dict(zip(weight.keys(),lines))
+    
+    #drwa the graph
     plt.figure(figsize=(20,20))
     nx.draw_networkx_nodes(G,pos,node_color='grey',
                           node_size=500,alpha=0.8)
@@ -44,9 +62,6 @@ if __name__ == '__main__':
         for j in range(i+1,len(L)):
             L[i][j] = random.random()
   
-    #load labels
-    file = open('labels/caltech_101_classes.txt','r')
-    lines = file.readline().strip().split(',')
-    labels = dict(zip(list(range(len(L))),lines))
-  
-    visualize(L, labels)
+
+    visualize(L)
+
